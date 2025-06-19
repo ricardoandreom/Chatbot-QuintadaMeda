53ffd3845ba1f2a67fbdca3d629f7edaee8fbe63
ricardoandre <ricardo.andreom@gmail.com>
2025-06-20 00:38:07 +0100

    add some final settings

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Load variables do .env
-load_dotenv()
+#load_dotenv()
 # api_key from secrets
 api_key = st.secrets["openai"]["api_key"]
