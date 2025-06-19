8551178885be50470dfb3131ed78f8c9523ace99
ricardoandre <ricardo.andreom@gmail.com>
2025-06-20 00:54:30 +0100

    add some final settings

--- rag/retriever.py
+++ rag/retriever.py
@@ -9,6 +9,7 @@ from config import api_key
 def create_retriever(documents, persist_path="faiss_index"):
     docs = [Document(page_content=d["content"], metadata={"source": d["source"]}) for d in documents]
     embedding = OpenAIEmbeddings(
+            api_key=api_key,
             model="text-embedding-3-large",
             )
     vectorstore = FAISS.from_documents(docs, embedding)
